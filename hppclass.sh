ab9b50d5b908fa4a07cfb6b667ea0f5ff6dae1c4
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-05-27 11:07:02 +0200

    hpp definition add

--- hppclass.sh
+++ hppclass.sh
@@ -2,15 +2,20 @@
 
 FILENAME=$1
 CLASS=$2
+upper=$(printf "%s" "$1" | tr '[:lower:]' '[:upper:]'| tr '.' '_')
 
 cat > $FILENAME << EOF
+#ifndef $upper
+# define $upper
 
 class $CLASS
 {
-public:
-    $CLASS();
-	$CLASS(const $CLASS& other); 
-	$CLASS& operator=(const $CLASS& other);
-    ~$CLASS();
+	public:
+    	$CLASS();
+		$CLASS(const $CLASS& other); 
+		$CLASS& operator=(const $CLASS& other);
+		~$CLASS();
 };
+
+#endif
 EOF
